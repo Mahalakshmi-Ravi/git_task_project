7ddb5e46ad7669385fde2cbd899a10e2a7df7e47
Mahalakshmi-Ravi <154545376+Mahalakshmi-Ravi@users.noreply.github.com>
2024-03-07 15:05:29 +0000

    Create calculator.py

---
+++ calculator.py
@@ -0,0 +1,24 @@
+# calculator.py
+
+import math
+
+class SimpleCalculator:
+    def __init__(self):
+        pass
+
+    def add(self, x, y):
+        return x + y
+
+    def subtract(self, x, y):
+        return x - y
+
+    def multiply(self, x, y):
+        return x * y
+
+    def divide(self, x, y):
+        if y != 0:
+            return x / y
+        else:
+            raise ValueError("Cannot divide by zero")
+
+
